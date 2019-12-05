ca733815c48913f4bd7a3a3b0de80d02b75d1000
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-12-05 00:38:38 -0500

    finished hangman project

--- lib/play_hangman.rb
+++ lib/play_hangman.rb
@@ -8,6 +8,7 @@ require_relative "hangman"
 hangman = Hangman.new
 
 until hangman.game_over?
+  system("clear")
   puts "Incorrect Guesses Remaining: #{hangman.remaining_incorrect_guesses}"
   puts "Attempted Chars: #{hangman.attempted_chars}"
   puts "Word: #{hangman.guess_word}"
